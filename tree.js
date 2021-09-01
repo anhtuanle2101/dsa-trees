4d16e7429d243fbae70518afc3fe89a4fcc754ac
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-09-01 12:03:43 -0400

    n-ary trees

--- tree.js
+++ tree.js
@@ -15,20 +15,64 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let sum = 0;
+    if (this.root === null){
+      return sum;
+    }
+    if (this.root.children.length===0){
+      return this.root.val;
+    }
+    const stack = [this.root]
+    while (stack.length){
+      const currentNode = stack.pop();
+      sum += currentNode.val;
+      //console.log(currentNode);
+      for (let child of currentNode.children){
+        stack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    let count = 0;
+    if (this.root === null){
+      return count;
+    }
+    const queue = [this.root];
+    while (queue.length){
+      const currentNode = queue.shift();
+      if (currentNode.val%2==0){
+        count+=1;
+      }
+      for (let child of currentNode.children){
+        queue.push(child);
+      }
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    let count = 0;
+    if (this.root === null){
+      return count;
+    }
+    const stack = [this.root];
+    while (stack.length){
+      const currentNode = stack.pop();
+      if (currentNode.val > lowerBound){
+        count++;
+      }
+      for (let child of currentNode.children){
+        stack.push(child);
+      }
+    }
+    return count;
   }
 }
 
